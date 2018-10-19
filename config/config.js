53abf158e4b92e32cc321027cd300f9cc11a3640
boyzhaotian <13426031783@139.com>
2018-10-19 17:07:00 +0800

    'mail'

---
+++ config/config.js
@@ -0,0 +1,5 @@
+// 数据库地址： 'mongodb://用户名:密码@ip地址:端口号/数据库';
+module.exports = {
+    mongodb: 'mongodb://localhost:27017/data',
+    // mongodb: 'mongodb://118.89.234.18:27017/data',
+}
---
+++ schemas/UserSchema.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+//创建Schema
+var userSchema = new Schema({
+    username:String,
+    password:String
+});
+module.exports = userSchema;
